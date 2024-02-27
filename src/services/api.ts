2e85a472153a3f1171178556ea04df00b02a56db
Vitor Sales <vitormalisa@gmail.com>
2024-02-27 13:25:14 -0300

    Requisito 1: função getProductsFromCategoryAndQuery implementada. Requisito 1 finalizado.

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,8 +5,16 @@ export async function getCategories() {
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (categoryId) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const data = await response.json();
+    return data;
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductById() {
